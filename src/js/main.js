3e4ed503b628038587aa0817d20db9ee0dbfb2da
van-yog <ikharchenko2008@gmail.com>
2021-01-24 17:04:20 +0300

    mobile fersion with toggle menu

--- src/js/main.js
+++ src/js/main.js
@@ -7,4 +7,14 @@ mobileBtn.addEventListener("click", () => {
   let menu = document.querySelector(".menu");
   console.log(menu);
   menu.classList.toggle("show");
+  let icon = document.querySelector(".mobile__icon");
+  icon.classList.toggle("mobile__icon-open");
+});
+
+let submenu = document.querySelector(".submenu");
+
+submenu.addEventListener("touchstart", () => {
+  console.log("touch");
+  let submenu = document.querySelector(".submenu__dnone");
+  submenu.classList.toggle("show");
 });
